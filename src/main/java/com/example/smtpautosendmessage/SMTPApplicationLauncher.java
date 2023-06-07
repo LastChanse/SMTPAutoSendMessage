fb5a8fb417b9237e0e47ba2c38cbf9a4c0f6ce4a
Георгий Кутасов <gera.kutasov227@gmail.com>
2023-06-07 14:12:09 +0300

    change

--- src/main/java/com/example/smtpautosendmessage/SMTPApplicationLauncher.java
+++ src/main/java/com/example/smtpautosendmessage/SMTPApplicationLauncher.java
@@ -1,5 +1,9 @@
 package com.example.smtpautosendmessage;
 
+/** Класс служит для запуска графического приложения без использования внешней папки jdk-19
+ @author LastChanse
+ @version 0.1.0
+ */
 public class SMTPApplicationLauncher {
     public static void main(String[] args) {
         SMTPApplication.main(args);
